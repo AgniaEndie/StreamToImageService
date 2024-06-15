19355608a71d4d1847d3d0d7821b6e75552db082
agniaendie <endienasg3@gmail.com>
2024-06-15 19:10:25 +0500

    git fix

--- src/StreamToImageService.py
+++ src/StreamToImageService.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/camera/<id>')
 def stream_to_image(id):
-    # test
+    # test23
     cameras = requests.get('http://camera-registry-service/get/all').json()
     for camera in cameras:
         if camera[0] == id:
