bad91cae48ff3f33e061732dad33d9fb56eec251
agniaendie <endienasg3@gmail.com>
2024-06-15 17:48:58 +0500

    test ci2

--- src/StreamToImageService.py
+++ src/StreamToImageService.py
@@ -6,10 +6,13 @@ app = Flask(__name__)
 
 @app.route('/camera/<id>')
 def stream_to_image(id):
-    #test
+    # test
     cameras = requests.get('http://camera-registry-service/get/all').json()
     for camera in cameras:
         if camera["uuid"] == id:
             return requests.get(camera['external_ip'])
         else:
             return Response("", 500)
+
+
+app.run("0.0.0.0", host=8080, debug=True)
